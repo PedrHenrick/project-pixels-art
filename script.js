630fdfbe7834bceff20a99878597a8fe8259243c
Pedro <phmc@ic.ufal.br>
2021-11-22 20:24:17 -0300

    Atualização do requisito 6 e iniciando requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-// npm run lint
-// npm run lint:styles
-// npm run cypress:open
+window.onload = function () {
+    let color = document.querySelectorAll('.color');
+
+    // adicionando as cores;
+    for (let key = 0; key < color.length; key +=1) {
+    color[key].style.backgroundColor = color[key].classList[1];
+    }
+    let verificador = false;
+
+    for (let key of color){
+        key.addEventListener('click', function(event){
+            let selected = document.querySelector('.selected');
+            selected.classList.remove('selected')
+            event.target.classList.add('selected'); 
+        });
+    }
+}
