f6fcee834d06485cf8d326b685b6781849f0e594
Pedro <phmc@ic.ufal.br>
2021-11-23 01:31:44 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -28,10 +28,61 @@ for (const key of pixels) {
 }
 
 // configurando botão clear
-const bt = document.querySelector('button');
-bt.addEventListener('click', ClearPixelBox);
+const btLimpar = document.querySelector('button');
+btLimpar.addEventListener('click', ClearPixelBox);
 function ClearPixelBox() {
     for (const key of pixels) {
         key.style.backgroundColor = 'white';
     }
+}
+
+// configurando input e botão
+let input = document.querySelector('input');
+let btAlterar  = document.querySelector('#generate-board');
+
+input.addEventListener('keyup', teclado);
+function teclado(event) {
+    const pixels = document.querySelectorAll('.pixel');
+  if (event.key === 'Enter' && input.value > 0) {
+    if (input.value < 5) {
+      for (const key of pixels) {
+        key.style.width = 5 * 5 + 'px';
+        key.style.height = 5 * 5 + 'px'
+      }
+    } else if (input.value >= 50) {
+      for (const key of pixels) {
+        key.style.width = 50 * 50 + 'px';
+        key.style.height = 50 * 50 + 'px'
+      }
+    } else {
+      for (const key of pixels) {
+        key.style.width = input.value * input.value + 'px';
+        key.style.height = input.value * input.value + 'px'
+      }
+    }
+  }
+}
+btAlterar.addEventListener('click', conferirInput)
+function conferirInput() {
+    const pixels = document.querySelectorAll('.pixel');
+  if (input.value > 0) {
+    if (input.value <= 5) {
+      for (const key of pixels) {
+        key.style.width = 5 * 5 + 'px';
+        key.style.height = 5 * 5 + 'px'
+      }
+    } else if (input.value >= 50) {
+      for (const key of pixels) {
+          key.style.width = 50 * 50 + 'px';
+          key.style.height = 50 * 50 + 'px'
+        }
+      } else {
+        for (const key of pixels) {
+          key.style.width = input.value * input.value + 'px';
+          key.style.height = input.value * input.value + 'px'
+        }
+      }
+    } else {
+      alert ("Board inválido!");
+  }
 }
