d288c749541150cc5c43efddcd3cae6ce65b4054
Pedro <phmc@ic.ufal.br>
2021-11-23 15:08:31 -0300

    Teste de lint

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Campo HTML
+const divBoard = document.createElement('div');
+document.body.appendChild(divBoard);
+divBoard.id = 'pixel-board';
+
+for (let i = 0; i < 5; i += 1) {
+  const boards = document.querySelector('#pixel-board');
+  const divLine = document.createElement('div');
+  boards.appendChild(divLine);
+  divLine.className = 'line';
+}
+for (let j = 0; j < 5; j += 1) {
+  for(let i = 0; i < 5; i += 1) {
+    let line = document.querySelectorAll('.line')
+    let divPixel = document.createElement('div');
+    line[j].appendChild(divPixel);
+    divPixel.className = 'pixel'
+  }
+}
+
 // Campo CSS
 // adicionando as cores na paleta;
 const color = document.querySelectorAll('.color');
@@ -6,26 +26,6 @@ for (const key of color) {
 }
 // Campo JavaScript e CSS
 // campo de declaração de variáveis
-const pixels = document.querySelectorAll('.pixel');
-
-// Mudança na cor
-for (const key of color) {
-  key.addEventListener('click', selectedColor);
-  function selectedColor(event) {
-    const selected = document.querySelector('.selected');
-    selected.classList.remove('selected');
-    event.target.classList.add('selected'); 
-  }
-}
-
-// adicionando cor aos pixels
-for (const key of pixels) {
-  key.addEventListener('click', selectedPixel);
-  function selectedPixel(event) {
-    const selected = document.querySelector('.selected');
-    event.target.style.backgroundColor = selected.style.backgroundColor;
-  };
-}
 
 // configurando botão clear
 const btLimpar = document.querySelector('button');
@@ -43,9 +43,6 @@ let btAlterar  = document.querySelector('#generate-board');
 input.addEventListener('keyup', teclado);
 function teclado(event) {
   if (event.key === 'Enter' && input.value > 0) {
-    for (const key of pixels) {
-        key.style.backgroundColor = 'white';
-    }
     if (input.value <= 5) {
       var lineDivs = 5;
       var pixelsDiv = 5;
@@ -57,6 +54,7 @@ function teclado(event) {
       var pixelsDiv = input.value; 
     }
     let boards = document.querySelector('#pixel-board').remove();
+
     const divBoard = document.createElement('div');
     document.body.appendChild(divBoard);
     divBoard.id = 'pixel-board';
@@ -78,41 +76,61 @@ function teclado(event) {
   }
 }
 btAlterar.addEventListener('click', conferirInput)
+let lineDivs;
+let pixelsDiv;
 function conferirInput() {
   if (input.value > 0) {
-    for (const key of pixels) {
-        key.style.backgroundColor = 'white';
-    }
     if (input.value <= 5) {
-      var lineDivs = 5;
-      var pixelsDiv = 5;
+      lineDivs = 5;
+      pixelsDiv = 5;
     } else if (input.value >= 50) {
-      var lineDivs = 50;
-      var pixelsDiv = 50;  
+      lineDivs = 50;
+      pixelsDiv = 50;  
     } else {
-      var lineDivs = input.value;
-      var pixelsDiv = input.value; 
+      lineDivs = input.value;
+      pixelsDiv = input.value; 
     }
     let boards = document.querySelector('#pixel-board').remove();
-    const divBoard = document.createElement('div');
-    document.body.appendChild(divBoard);
-    divBoard.id = 'pixel-board';
+    const divBoards = document.createElement('div');
+    document.body.appendChild(divBoards);
+    divBoards.id = 'pixel-board';
     
     for (let i = 0; i < lineDivs; i += 1) {
-      const boards = document.querySelector('#pixel-board');
-      const divLine = document.createElement('div');
-      boards.appendChild(divLine);
-      divLine.className = 'line';
+      const boardss = document.querySelector('#pixel-board');
+      const divLines = document.createElement('div');
+      boards.appendChild(divLines);
+      divLines.className = 'line';
     }
     for (let j = 0; j < pixelsDiv; j += 1) {
       for(let i = 0; i < pixelsDiv; i += 1) {
-        let line = document.querySelectorAll('.line')
-        let divPixel = document.createElement('div');
-        line[j].appendChild(divPixel);
-        divPixel.className = 'pixel'
+        let lines = document.querySelectorAll('div .line')
+        let divPixels = document.createElement('div');
+        lines[j].appendChild(divPixels);
+        divPixels.className = 'pixel'
       }
     }
   } else {
     alert ("Board inválido!");
   }
+}
+
+  const pixels = document.querySelectorAll('.pixel');
+
+// Mudança na cor
+for (const key of color) {
+  key.addEventListener('click', selectedColor);
+  function selectedColor(event) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected'); 
+  }
+}
+
+// adicionando cor aos pixels
+for (const key of pixels) {
+  key.addEventListener('click', selectedPixel);
+  function selectedPixel(event) {
+    const selected = document.querySelector('.selected');
+    event.target.style.backgroundColor = selected.style.backgroundColor;
+  };
 }
