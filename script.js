61b44b3e25d186784063e0bdd0e037962ea02a09
Pedro <phmc@ic.ufal.br>
2021-11-23 00:50:11 -0300

    Requisito 9 2.0

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-window.onload = function pag () {
-  // Campo CSS
-  // adicionando as cores na paleta;
-  const color = document.querySelectorAll('.color');
-  for (const key of color) {
-    key.style.backgroundColor = key.classList[1];
+// Campo CSS
+// adicionando as cores na paleta;
+const color = document.querySelectorAll('.color');
+for (const key of color) {
+  key.style.backgroundColor = key.classList[1];
+}
+// Campo JavaScript e CSS
+// campo de declaração de variáveis
+const pixels = document.querySelectorAll('.pixel');
+
+// Mudança na cor
+for (const key of color) {
+  key.addEventListener('click', selectedColor);
+  function selectedColor(event) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected'); 
   }
+}
 
-  // Campo JavaScript e CSS
-  // campo de declaração de variáveis  
-  const pixels = document.querySelectorAll('.pixel');
+// adicionando cor aos pixels
+for (const key of pixels) {
+  key.addEventListener('click', selectedPixel);
+  function selectedPixel(event) {
+    const selected = document.querySelector('.selected');
+    event.target.style.backgroundColor = selected.style.backgroundColor;
+  };
+}
 
-  // Mudança na cor
-  for (const key of color) {
-    key.addEventListener('click', function selectedColor(event) {
-      const selected = document.querySelector('.selected');
-      selected.classList.remove('selected')
-      event.target.classList.add('selected'); 
-    });
-  }
-
-  // adicionando cor aos pixels
-  for (const key of pixels) {
-    key.addEventListener('click', function selectedPixel(event) {
-      const selected = document.querySelector('.selected');
-      event.target.style.backgroundColor = selected.style.backgroundColor;
-    });
-  }
-
-  // configurando botão clear
-  const bt = document.querySelector('button');
-  bt.addEventListener('click', function ClearPixelBox() {
+// configurando botão clear
+const bt = document.querySelector('button');
+bt.addEventListener('click', ClearPixelBox);
+function ClearPixelBox() {
     for (const key of pixels) {
-      key.style.backgroundColor = 'white';
-    };
-  });
+        key.style.backgroundColor = 'white';
+    }
 }
