945b4910f9723e2de2d3b64d57b923ddd7c4d1d9
Pedro <phmc@ic.ufal.br>
2021-11-22 21:26:34 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-window.onload = function () {
-    let color = document.querySelectorAll('.color');
-
-    // adicionando as cores;
-    for (let key = 0; key < color.length; key +=1) {
+window.onload = function() {
+  let color = document.querySelectorAll('.color');
+  let pixels = document.querySelectorAll('.pixel');
+  // adicionando as cores;
+  for (let key = 0; key < color.length; key +=1) {
     color[key].style.backgroundColor = color[key].classList[1];
-    }
-    let verificador = false;
-
-    for (let key of color){
-        key.addEventListener('click', function(event){
-            let selected = document.querySelector('.selected');
-            selected.classList.remove('selected')
-            event.target.classList.add('selected'); 
-        });
-    }
+  }
+  // Mudança na cor
+  for (let key of color){
+    key.addEventListener('click', function(event) {
+      let selected = document.querySelector('.selected');
+      selected.classList.remove('selected')
+      event.target.classList.add('selected'); 
+    });
+  }
+  // adicionando cor aos pixels
+  for (let key of pixels){
+    key.addEventListener('click', function(event) {
+      let selected = document.querySelector('.selected');
+      event.target.style.backgroundColor = selected.style.backgroundColor;
+    });
+  }
 }
