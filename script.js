d828edfbcd4af6034849273373c304b3785e172a
Pedro <phmc@ic.ufal.br>
2021-11-24 18:04:47 -0300

    Erro desconhecido no requisito 5. Código refatorado e passando nos demais

--- script.js
+++ script.js
@@ -1,98 +1,72 @@
 // Campo HTML
-let section = document.querySelector('section');
-const divBoard = document.createElement('div');
-section.appendChild(divBoard);
-divBoard.id = 'pixel-board';
-
-for (let i = 0; i < 5; i += 1) {
-  const boards = document.querySelector('#pixel-board');
-  const divLine = document.createElement('div');
-  boards.appendChild(divLine);
-  divLine.className = 'line';
-}
 for (let j = 0; j < 5; j += 1) {
+  const boards = document.querySelector('#pixel-board');
+  const sectionLine = document.createElement('section');
+  sectionLine.className = 'line';
+  boards.appendChild(sectionLine);
   for(let i = 0; i < 5; i += 1) {
-    let line = document.querySelectorAll('.line')
     let divPixel = document.createElement('div');
-    line[j].appendChild(divPixel);
-    divPixel.className = 'pixel'
-    divPixel.style.backgroundColor = 'white';
+    divPixel.className = 'pixel';
+    sectionLine.appendChild(divPixel);
   }
 }
-alert("Sua tabela se inicia em 5x5!");
 
 // Campo CSS
 // adicionando as cores na paleta;
-let colorOne = Math.round(Math.random()* 300);
-let colorTwo = Math.round(Math.random()* 300);
-let ColorTree = Math.round(Math.random()* 300);
-
-let newColorRGBTwo = `rgb(${colorOne}, ${colorTwo}, ${ColorTree})`;
-let newColorRGBTree = `rgb(${colorOne}, ${ColorTree}, ${colorTwo})`;
-let newColorRGBFour = `rgb(${colorTwo}, ${colorOne}, ${ColorTree})`;
+let newColorRGB2 = `rgb(${Math.round(Math.random()* 300)}, ${Math.round(Math.random()* 300)}, ${Math.round(Math.random()* 300)})`;
+let newColorRGB3 = `rgb(${Math.round(Math.random()* 300)}, ${Math.round(Math.random()* 300)}, ${Math.round(Math.random()* 300)})`;
+let newColorRGB4 = `rgb(${Math.round(Math.random()* 300)}, ${Math.round(Math.random()* 300)}, ${Math.round(Math.random()* 300)})`;
 
 let arrayColor = [];
 arrayColor.push('black');
-arrayColor.push(newColorRGBTwo);
-arrayColor.push(newColorRGBTree);
-arrayColor.push(newColorRGBFour);
+arrayColor.push(newColorRGB2);
+arrayColor.push(newColorRGB3);
+arrayColor.push(newColorRGB4);
 
 const color = document.querySelectorAll('.color');
 for (let i = 0; i < color.length; i += 1) {
   color[i].style.backgroundColor = arrayColor[i];
 }
-// Campo JavaScript e CSS
-// campo de declaração de variáveis
 
-// configurando botão clear
+// Campo JavaScript
+// Configurando botão clear
+const pixel = document.querySelectorAll('div, #pixel-board');
 const btLimpar = document.querySelector('button');
 btLimpar.addEventListener('click', ClearPixelBox);
 function ClearPixelBox() {
-    for (const key of pixels) {
+    for (const key of pixel) {
         key.style.backgroundColor = 'white';
     }
 }
 
-// configurando input e botão
+// Variáveis
+let lengthDivs;
 let btAlterar  = document.querySelector('#generate-board');
 let input = document.querySelector('#board-size');
 
+// Configurando input e botão
 btAlterar.addEventListener('click', conferirInput)
-let lineDivs;
-let pixelsDiv;
 function conferirInput() {
+  // Verificação de valor
   if (input.value > 0) {
-    if (input.value < 5) {
-      lineDivs = 5;
-      pixelsDiv = 5;
-      alert(`Tamanho mínimo ultrapaçado, como padrão usaremos ${lineDivs}x${pixelsDiv}!`);
-    } else if (input.value > 50) {
-      lineDivs = 50;
-      pixelsDiv = 50;  
-      alert(`Tamanho máximo ultrapaçado, como padrão usaremos ${lineDivs}x${pixelsDiv}!`);
-    } else {
-      lineDivs = input.value;
-      pixelsDiv = input.value;
-      alert(`Você alterou sua tabela para ${input.value}x${input.value} !`); 
-    }
-    let boards = document.querySelector('#pixel-board').remove();
-    const divBoard = document.createElement('div');
-    section.appendChild(divBoard);
-    divBoard.id = 'pixel-board';
+    if (input.value < 5) lengthDivs = 5;
+    else if (input.value > 50) lengthDivs = 50;
+    else lengthDivs = input.value;
 
-    for (let i = 0; i < lineDivs; i += 1) {
+    // Removendo div antiga
+    let removeBoards = document.querySelectorAll('.line');
+    for (const key of removeBoards) key.remove();
+    
+    // Adicionando line e div com tamanho selecionado
+    for (let j = 0; j < lengthDivs; j += 1) {
       const boards = document.querySelector('#pixel-board');
-      const divLine = document.createElement('div');
-      boards.appendChild(divLine);
-      divLine.className = 'line';
-    }
-    for (let j = 0; j < pixelsDiv; j += 1) {
-      for(let g = 0; g < pixelsDiv; g += 1) {
-        let line = document.querySelectorAll('.line')
+      const sectionLine = document.createElement('section');
+      sectionLine.className = 'line';
+      boards.appendChild(sectionLine);
+      for(let i = 0; i < lengthDivs; i += 1) {
         let divPixel = document.createElement('div');
-        line[j].appendChild(divPixel);
-        divPixel.className = 'pixel'
-        divPixel.style.backgroundColor = 'white';
+        divPixel.className = 'pixel';
+        sectionLine.appendChild(divPixel);
       }
     }
   } else {
@@ -100,23 +74,25 @@ function conferirInput() {
   }
 }
 
-  const pixels = document.querySelectorAll('.pixel');
+// Variáveis
+const pixels = document.querySelector('div, #pixel-board');
+const selectedPosicion = document.querySelector('#color-palette');
 
-// Mudança na cor
-for (const key of color) {
-  key.addEventListener('click', selectedColor);
-  function selectedColor(event) {
-    const selected = document.querySelector('.selected');
-    selected.classList.remove('selected');
-    event.target.classList.add('selected'); 
-  }
+// Seleção de cor
+function colorPalette() {
+  selectedPosicion.addEventListener('click', function(event) {
+      const selected = document.querySelector('.selected')
+      selected.classList.remove('selected');
+      event.target.classList.add('selected');        
+  })
 }
+colorPalette();
 
-// adicionando cor aos pixels
-for (const key of pixels) {
-  key.addEventListener('click', selectedPixel);
-  function selectedPixel(event) {
-    const selected = document.querySelector('.selected');
-    event.target.style.backgroundColor = selected.style.backgroundColor;
-  };
+// Adicionando cor aos pixels
+function colorPixel() {
+  pixels.addEventListener('click', function(event) {
+      const selected = document.querySelector('.selected')
+      event.target.style.backgroundColor = selected.style.backgroundColor;       
+  })
 }
+colorPixel();
